97c98fce40913558f38d87b34f16be7bab9337ba
Sunyoung <d2tectiv2@naver.com>
2025-12-03 15:04:01 +0900

    ♻️ 리팩토링: Token 유틸 타입스크립트 적용 (타입 가드 활용)

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,7 +1,12 @@
 import jwt from 'jsonwebtoken';
-import { JWT_ACCESS_TOKEN_SECRET, JWT_REFRESH_TOKEN_SECRET } from './constants';
+import { Request } from 'express';
+import {
+  ACCESS_TOKEN_COOKIE_NAME,
+  JWT_ACCESS_TOKEN_SECRET,
+  JWT_REFRESH_TOKEN_SECRET,
+} from './constants';
 
-function generateTokens(userId) {
+function generateTokens(userId: string) {
   const accessToken = jwt.sign({ id: userId }, JWT_ACCESS_TOKEN_SECRET, {
     expiresIn: '1h',
   });
@@ -11,22 +16,32 @@ function generateTokens(userId) {
   return { accessToken, refreshToken };
 }
 
-function verifyAccessToken(token) {
+function verifyAccessToken(token: string) {
   const decoded = jwt.verify(token, JWT_ACCESS_TOKEN_SECRET);
+  if (typeof decoded === 'string') {
+    throw new Error('Invalid token payload');
+  }
   return { userId: decoded.id };
 }
 
-function verifyRefreshToken(token) {
+function verifyRefreshToken(token: string) {
   const decoded = jwt.verify(token, JWT_REFRESH_TOKEN_SECRET);
+  if (typeof decoded === 'string') {
+    throw new Error('Invalid token payload');
+  }
   return { userId: decoded.id };
 }
 
-export function getUserIdFromToken(req) {
+export function getUserIdFromToken(req: Request) {
   const token = req.cookies[ACCESS_TOKEN_COOKIE_NAME];
   if (!token) return null;
 
   try {
     const decoded = jwt.verify(token, JWT_ACCESS_TOKEN_SECRET);
+    if (typeof decoded === 'string') {
+      throw new Error('Invalid token payload');
+    }
+
     return decoded.id;
   } catch (error) {
     return null;

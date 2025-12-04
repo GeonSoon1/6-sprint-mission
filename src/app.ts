7a8d545d30b2dbb9213d95cb8c7f8e389b5ebbd5
Sunyoung <d2tectiv2@naver.com>
2025-12-04 14:29:51 +0900

    ♻️ 리팩토링: 서버 실행 시 포트 번호 출력되도록 수정

--- src/app.ts
+++ src/app.ts
@@ -32,4 +32,4 @@ app.use('/users', usersRouter);
 app.use(defaultNotFoundHandler);
 app.use(errorHandlerMiddleware);
 
-app.listen(PORT, () => console.log('서버 시작'));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));

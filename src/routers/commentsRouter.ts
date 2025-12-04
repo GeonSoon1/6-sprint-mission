695fd550cff1da782248e6ced3404998b41e35a3
Sunyoung <d2tectiv2@naver.com>
2025-12-04 13:28:14 +0900

    ♻️ 리팩토링: 댓글(Comments) 라우터 및 검증 스키마 타입스크립트 적용 (파라미터 검증 추가)

--- src/routers/commentsRouter.ts
+++ src/routers/commentsRouter.ts
@@ -3,7 +3,11 @@ import * as commentsController from '../controllers/commentsController';
 import { validateCursorPagination } from '../middlewares/validateCursorPagination';
 import { authenticate } from '../middlewares/authenticate';
 import { validate } from '../middlewares/validate';
-import { CreateCommentSchema, PatchCommentSchema } from '../validations/commentsSchema';
+import {
+  CreateCommentSchema,
+  PatchCommentSchema,
+  CommentParamsSchema,
+} from '../validations/commentsSchema';
 
 const router = Router({ mergeParams: true });
 
@@ -19,10 +23,16 @@ router.get('/', validateCursorPagination, commentsController.getComments);
 router.patch(
   '/:commentId',
   authenticate,
+  validate(CommentParamsSchema, 'params'),
   validate(PatchCommentSchema, 'body'),
   commentsController.patchComment,
 );
 
-router.delete('/:commentId', authenticate, commentsController.deleteComment);
+router.delete(
+  '/:commentId',
+  authenticate,
+  validate(CommentParamsSchema, 'params'),
+  commentsController.deleteComment,
+);
 
 export default router;
--- src/validations/commentsSchema.ts
+++ src/validations/commentsSchema.ts
@@ -1,7 +1,7 @@
 import * as s from 'superstruct';
 import isUUID from 'is-uuid';
 
-export const UUID = s.define('UUID', (value) => isUUID.v4(value));
+export const UUID = s.define('UUID', (value) => typeof value === 'string' && isUUID.v4(value));
 
 export const CreateCommentSchema = s.object({
   content: s.size(s.string(), 1, 500),
@@ -10,5 +10,5 @@ export const CreateCommentSchema = s.object({
 export const PatchCommentSchema = s.partial(CreateCommentSchema);
 
 export const CommentParamsSchema = s.object({
-  id: UUID,
+  commentId: UUID,
 });

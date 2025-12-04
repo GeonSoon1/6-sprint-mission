0d77d8df9d318d4609576505849fdb73603dd867
Sunyoung <d2tectiv2@naver.com>
2025-12-04 11:44:35 +0900

    ♻️ 리팩토링: 페이지네이션 미들웨어 타입스크립트 적용 (타입 가드를 통한 안전성 확보)

--- src/middlewares/validateCursorPagination.ts
+++ src/middlewares/validateCursorPagination.ts
@@ -1,26 +1,33 @@
 import isUUID from 'is-uuid';
+import { Request, Response, NextFunction } from 'express';
+import { ErrorWithStatus } from '../utils/types';
 
-export const validateCursorPagination = (req, res, next) => {
+export const validateCursorPagination = (req: Request, res: Response, next: NextFunction) => {
   const { cursor, limit } = req.query;
 
   let _limit = 10;
-  if (limit !== undefined) {
-    _limit = parseInt(limit);
-    if (isNaN(_limit) || _limit <= 0) {
-      const error = new Error('limit 쿼리 파라미터는 0보다 큰 정수여야 합니다.');
+
+  if (typeof limit === 'string') {
+    const parsedLimit = parseInt(limit);
+
+    if (isNaN(parsedLimit) || parsedLimit <= 0) {
+      const error: ErrorWithStatus = new Error('limit 쿼리 파라미터는 0보다 큰 정수여야 합니다.');
+      error.status = 400;
+      throw error;
+    }
+    _limit = parsedLimit;
+  }
+
+  if (cursor) {
+    if (typeof cursor === 'string' && !isUUID.v4(cursor)) {
+      const error: ErrorWithStatus = new Error('cursor 쿼리 파라미터는 올바른 UUID여야 합니다.');
       error.status = 400;
       throw error;
     }
   }
 
-  if (cursor && !isUUID.v4(cursor)) {
-    const error = new Error('cursor 쿼리 파라미터는 올바른 UUID여야 합니다.');
-    error.status = 400;
-    throw error;
-  }
-
   req.paginationParams = {
-    cursor: cursor || null,
+    cursor: typeof cursor === 'string' ? cursor : undefined,
     limit: _limit,
   };
 

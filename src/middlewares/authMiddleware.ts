1528d098d9e10017e12d184a2eebde9be400e245
Sunyoung <d2tectiv2@naver.com>
2025-12-08 14:03:24 +0900

    🐛 버그수정: 인증 미들웨어 DB 조회 로직 추가 및 req.user 타입 에러 해결

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,8 +1,9 @@
 import jwt from 'jsonwebtoken';
 import { ACCESS_TOKEN_COOKIE_NAME, JWT_ACCESS_TOKEN_SECRET } from '../utils/constants';
 import { RequestHandler } from 'express';
+import { prisma } from '../utils/prisma';
 
-export const authMiddleware: RequestHandler = (req, res, next) => {
+export const authMiddleware: RequestHandler = async (req, res, next) => {
   const token = req.cookies[ACCESS_TOKEN_COOKIE_NAME];
 
   if (!token) {
@@ -11,8 +12,16 @@ export const authMiddleware: RequestHandler = (req, res, next) => {
   try {
     const decoded = jwt.verify(token, JWT_ACCESS_TOKEN_SECRET);
 
-    if (typeof decoded === 'object' && decoded !== null) {
-      req.user = decoded;
+    if (typeof decoded === 'object' && decoded !== null && 'userId' in decoded) {
+      const userId = (decoded as any).userId;
+
+      const user = await prisma.user.findUnique({
+        where: { id: userId },
+      });
+
+      if (user) {
+        req.user = user;
+      }
     }
   } catch (error) {}
   next();

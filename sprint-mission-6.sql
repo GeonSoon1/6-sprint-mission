ad941d1de7972c102b2f9b2a7f097cdbb1f22fb4
kimjs <kimjs910522@gmail.com>
2025-12-12 17:31:58 +0900

    feat : SQL 중급 문제 완료

--- sprint-mission-6.sql
+++ sprint-mission-6.sql
@@ -63,18 +63,83 @@ WHERE name LIKE '%Cheese%' OR name LIKE '%Chicken%';
 -- # 중급 문제
 
 -- 1. `order_details` 테이블에서 각 피자(`pizza_id`)별로 주문된 건 수(`order_id`)를 보여주세요.
+SELECT 
+    pizza_id,
+    COUNT(order_id) AS order_count
+FROM order_details
+GROUP BY pizza_id; 
+
+
 
 -- 2. `order_details` 테이블에서 각 피자(`pizza_id`)별로 총 주문 수량을 구하세요.
+SELECT 
+    pizza_id,
+    SUM(quantity) AS total_quantity
+FROM order_details
+GROUP BY pizza_id; 
+
+
 
 -- 3. `pizzas` 테이블에서 `price`의 크기가 20보다 큰 피자의 종류만 `order_details` 테이블에서 조회하세요. (힌트: 서브쿼리)
+SELECT * 
+FROM order_details
+WHERE pizza_id in (
+    SELECT id
+    FROM pizzas
+    WHERE price > 20
+    )
+ORDER BY id DESC;
+
+
 
 -- 4. `orders` 테이블에서 각 날짜별 총 주문 건수를 `order_count` 라는 이름으로 계산하고, 하루 총 주문 건수가 80건 이상인 날짜만 조회한 뒤, 주문 건수가 많은 순서대로 정렬하세요.
+SELECT 
+    date,
+    COUNT(id) AS order_count
+FROM orders
+GROUP BY date
+HAVING COUNT(id) >= 80
+ORDER BY COUNT(id) DESC; 
+
+
+
 
 -- 5. `order_details` 테이블에서 피자별(`pizza_id`) 총 주문 수량이 10개 이상인 피자만 조회하고, 총 주문 수량 기준으로 내림차순 정렬하세요.
+SELECT 
+    pizza_id,
+    SUM(quantity) AS total_quantity
+FROM order_details
+GROUP BY pizza_id
+HAVING SUM(quantity) >= 10
+ORDER BY total_quantity DESC; 
+
+
 
 -- 6. `order_details` 테이블에서 피자별 총 수익을 `total_revenue` 라는 이름으로 구하세요. (수익 = `quantity * price`)
+SELECT 
+    order_details.pizza_id,
+    SUM(order_details.quantity) AS total_quantity,
+    SUM(order_details.quantity * pizzas.price) AS total_revenue
+FROM order_details JOIN pizzas
+ON pizzas.id = order_details.pizza_id
+GROUP BY pizza_id, pizzas.price; 
+
+
 
 -- 7. 날짜별로 피자 주문 건수(`order_count`)와 총 주문 수량(`total_quantity`)을 구하세요.
+
+SELECT 
+    orders.date,
+    COUNT(order_details.order_id) AS total_orders,
+    SUM(order_details.quantity) AS total_quantity
+FROM order_details JOIN orders
+ON orders.id = order_details.order_id
+GROUP BY orders.date
+ORDER BY orders.date DESC; 
+-- 보기 좋게 하기 위해서 
+-- total_orders = 오더 총 수량을 추가 데이터로 넣고
+-- 최신 날짜 순으로 정렬 하였습니다.
+
 
 
 -- # 고급 문제

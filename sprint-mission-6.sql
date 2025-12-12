3f2444417baf083e07503557a55e99e0fe6ae344
Dongwon Jeong <tunajjang@gmail.com>
2025-12-12 15:50:44 +0900

    feat: re-commit

--- sprint-mission-6.sql
+++ sprint-mission-6.sql
@@ -43,22 +43,24 @@ SELECT pizza_id, SUM(quantity) FROM order_details GROUP BY pizza_id;
 SELECT * FROM order_details WHERE (SELECT price FROM pizzas WHERE order_details.pizza_id = pizzas.id) > 20;
 
 -- 4. `orders` 테이블에서 각 날짜별 총 주문 건수를 `order_count` 라는 이름으로 계산하고, 하루 총 주문 건수가 80건 이상인 날짜만 조회한 뒤, 주문 건수가 많은 순서대로 정렬하세요.
-
+SELECT date, COUNT(id) AS order_count FROM orders GROUP BY date HAVING COUNT(id) >= 80 ORDER BY order_count DESC;
 
 
 -- 5. `order_details` 테이블에서 피자별(`pizza_id`) 총 주문 수량이 10개 이상인 피자만 조회하고, 총 주문 수량 기준으로 내림차순 정렬하세요.
-SELECT pizza_id, count(*) FROM order_details;
+SELECT pizza_id, SUM(quantity) AS total_orders FROM order_details GROUP BY pizza_id HAVING SUM(quantity) >= 10 ORDER BY total_orders DESC;
 
 -- 6. `order_details` 테이블에서 피자별 총 수익을 `total_revenue` 라는 이름으로 구하세요. (수익 = `quantity * price`)
+SELECT pizza_id, CAST(SUM(quantity * price) AS NUMERIC(10, 2)) AS total_revenue FROM order_details JOIN pizzas ON pizzas.id = order_details.pizza_id GROUP BY pizza_id;
 
 -- 7. 날짜별로 피자 주문 건수(`order_count`)와 총 주문 수량(`total_quantity`)을 구하세요.
+SELECT date, COUNT(DISTINCT orders.id) AS order_count, SUM(order_details.quantity) AS total_quantity FROM orders LEFT JOIN order_details ON orders.id = order_details.order_id GROUP BY date ORDER BY date ASC;
 
 
 -- # 고급 문제
 
 /*
     1. 피자별(`pizzas.id` 기준) 판매 수량 순위에서 피자별 판매 수량 상위에 드는 베스트 피자를 10개를 조회해 주세요. `pizzas`의 모든 컬럼을 조회하면서 각 피자에 해당하는 판매량을 `total_quantity`라는 이름으로 함께 조회합니다.
-        
+
         출력 예시:
 
         ```sql
@@ -75,6 +77,7 @@ SELECT pizza_id, count(*) FROM order_details;
         ```
 */
 
+SELECT pizzas.id, pizzas.type_id, pizzas.size, pizzas.price, SUM(order_details.quantity) AS total_quantity FROM pizzas JOIN order_details ON pizzas.id = order_details.pizza_id GROUP BY pizzas.id ORDER BY total_quantity DESC LIMIT 10;
 
 /*
     2. `orders` 테이블에서 2025년 3월의 일별 주문 수량을 `total_orders`라는 이름으로, 일별 총 주문 금액을 `total_amount`라는 이름으로 포함해서 조회하세요.
@@ -89,7 +92,12 @@ SELECT pizza_id, count(*) FROM order_details;
         2025-03-05 |           64 |  2350.650005340576
         ```
 */
-    
+SELECT date, COUNT(DISTINCT orders.id) AS total_orders, CAST(SUM(pizzas.price * order_details.quantity) AS NUMERIC (10, 2)) AS total_amount
+FROM orders
+LEFT JOIN order_details ON orders.id = order_details.order_id 
+LEFT JOIN pizzas ON order_details.pizza_id = pizzas.id
+WHERE date BETWEEN '2025-03-01' AND '2025-03-31'
+GROUP BY date;
 
 /*
     3. `order`의 `id`가 78에 해당하는 주문 내역들을 조회합니다. 주문 내역에서 각각 주문한 피자의 이름을 `pizza_name`, 피자의 크기를 `pizza_size`, 피자 가격을 `pizza_price`, 수량을 `quantity`, 각 주문 내역의 총 금액을 `total_amount` 라는 이름으로 조회해 주세요.
@@ -105,6 +113,12 @@ SELECT pizza_id, count(*) FROM order_details;
         The Four Cheese Pizza       | L          |       17.95 |        1 | 17.950000762939453
         ```
 */
+SELECT pizza_types.name AS pizza_name, pizzas.size AS pizza_size, pizzas.price AS pizza_price, order_details.quantity AS quantity, CAST(order_details.quantity * pizzas.price AS NUMERIC (10, 2)) AS total_amount 
+FROM order_details
+JOIN pizzas ON order_details.pizza_id = pizzas.id
+JOIN pizza_types ON pizzas.type_id = pizza_types.id
+WHERE order_details.order_id = 78
+ORDER BY pizzas.size ASC;
 
 
 /*    
@@ -120,6 +134,12 @@ SELECT pizza_id, count(*) FROM order_details;
         XXL  | 1006.6000213623047
         ```
 */
+
+SELECT pizzas.size, CAST(SUM(order_details.quantity * pizzas.price) AS NUMERIC (10, 2)) AS total_amount
+FROM order_details
+JOIN pizzas ON order_details.pizza_id = pizzas.id
+GROUP BY pizzas.size
+ORDER BY total_amount DESC;
 
 
 /*    
@@ -138,3 +158,9 @@ SELECT pizza_id, count(*) FROM order_details;
         ```
 */
 
+SELECT pizza_types.name, CAST(SUM(order_details.quantity * pizzas.price) AS NUMERIC (10, 2)) AS total_amount
+FROM order_details
+JOIN pizzas ON order_details.pizza_id = pizzas.id
+JOIN pizza_types ON pizzas.type_id = pizza_types.id
+GROUP BY pizza_types.name
+ORDER BY total_amount DESC;

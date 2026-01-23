ec3c07ef9f132f718dace9d39bd629adafae7375
kimjs <kimjs910522@gmail.com>
2026-01-23 14:32:31 +0900

    test : auth 라우터 테스트 진행 & auth structs 수정

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,16 @@ module.exports = {
   testMatch: ['**/*.test.ts'],
   transform: { '^.+\\.ts$': 'ts-jest' },
   moduleFileExtensions: ['ts', 'js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@controllers/(.*)$': '<rootDir>/src/controllers/$1',
+    '^@dto/(.*)$': '<rootDir>/src/dto/$1',
+    '^@lib/(.*)$': '<rootDir>/src/lib/$1',
+    '^@middleware/(.*)$': '<rootDir>/src/middleware/$1',
+    '^@repository/(.*)$': '<rootDir>/src/repository/$1',
+    '^@routers/(.*)$': '<rootDir>/src/routers/$1',
+    '^@service/(.*)$': '<rootDir>/src/service/$1',
+    '^@structs/(.*)$': '<rootDir>/src/structs/$1',
+    '^@app-types/(.*)$': '<rootDir>/src/types/$1',
+  },
 };
--- src/structs/auth.structs.ts
+++ src/structs/auth.structs.ts
@@ -1,14 +1,16 @@
-import { nonempty, nullable, object, string } from 'superstruct';
+import { nonempty, nullable, object, string, refine } from 'superstruct';
+import isEmail from 'is-email';
+
+const EmailStruct = refine(string(), 'Email', (value) => isEmail(value));
 
 export const RegisterBodyStruct = object({
-  email: nonempty(string()),
+  email: nonempty(EmailStruct),
   nickname: nonempty(string()),
   password: nonempty(string()),
   image: nullable(string()),
 });
 
 export const LoginBodyStruct = object({
-  email: nonempty(string()),
+  email: nonempty(EmailStruct),
   password: nonempty(string()),
 });
-

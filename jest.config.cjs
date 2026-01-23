fa5feff6e6b07c0c343be5d9200e384429aba27a
Jueun Lee <wndms328@naver.com>
2026-01-23 09:58:33 +0900

    Configure Jest coverage

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,21 +1,15 @@
 module.exports = {
-  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
   verbose: true,
   testMatch: ['**/*.test.ts'],
   transform: {
-    '^.+\\.ts$': [
-      'ts-jest',
-      {
-        useESM: true,
-        tsconfig: 'tsconfig.jest.json',
-      },
-    ],
+    '^.+\\.ts$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
   },
   moduleFileExtensions: ['ts', 'js'],
-  extensionsToTreatAsEsm: ['.ts'],
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
-  setupFilesAfterEnv: ['<rootDir>/tests/setupJest.ts'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts', '!src/main.ts', '!src/**/index.ts'],
+  coverageDirectory: 'coverage',
 };

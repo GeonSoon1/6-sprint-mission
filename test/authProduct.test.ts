498398fae29cb872cae484a478bd00a8ec534fe3
kimjs <kimjs910522@gmail.com>
2026-01-27 13:10:02 +0900

    refactor : authProduct 코드 정리

--- test/authProduct.test.ts
+++ test/authProduct.test.ts
@@ -4,6 +4,9 @@ import app from '@/app';
 import { userSample, productSample } from '@test/helper/mockdata';
 import { createProductsWithUsers } from '@test/helper/product';
 
+const agent1 = request.agent(app);
+const agent2 = request.agent(app);
+
 describe('인증이 필요한 상품 API 통합 테스트', () => {
   console.log('auth product run!!');
 
@@ -20,19 +23,23 @@ describe('인증이 필요한 상품 API 통합 테스트', () => {
     await prismaClient.user.deleteMany();
 
     // 사용자 식별 테스트를 위하여 임의로 user data 2개 입력
-    await request(app).post('/auth/register').send({
-      email: 'test@test.com',
+    await agent1.post('/auth/register').send({
+      email: 'test1@agent.com',
       nickname: 'tester',
       password: 'qwer1234',
       image: 'test.jpg',
     });
 
-    await request(app).post('/auth/register').send({
-      email: 'test2@test.com',
+    await agent1.post('/auth/login').send({ email: 'test1@agent.com', password: 'qwer1234' });
+
+    await agent2.post('/auth/register').send({
+      email: 'test2@agent.com',
       nickname: 'tester2',
       password: 'qwer1234',
       image: 'test.jpg',
     });
+
+    await agent2.post('/auth/login').send({ email: 'test2@agent.com', password: 'qwer1234' });
   });
 
   afterAll(async () => {
@@ -41,15 +48,7 @@ describe('인증이 필요한 상품 API 통합 테스트', () => {
 
   describe('인증을 거친 후 상품 생성 POST /products', () => {
     test('신규 상품 등록 : name, description, price, tags, images', async () => {
-      const agent = request.agent(app);
-
-      const user = await agent
-        .post('/auth/login')
-        .send({ email: 'test@test.com', password: 'qwer1234' });
-
-      expect(user.status).toBe(200);
-
-      const productResponse = await agent.post('/products').send({
+      const productResponse = await agent1.post('/products').send({
         name: '로봇',
         description: '어린이용 변신 로봇',
         price: 5000,
@@ -63,15 +62,7 @@ describe('인증이 필요한 상품 API 통합 테스트', () => {
     });
 
     test('신규 상품 등록 : tags & image 빈 배열', async () => {
-      const agent = request.agent(app);
-
-      const user = await agent
-        .post('/auth/login')
-        .send({ email: 'test@test.com', password: 'qwer1234' });
-
-      expect(user.status).toBe(200);
-
-      const productResponse = await agent.post('/products').send({
+      const productResponse = await agent1.post('/products').send({
         name: '로봇',
         description: '어린이용 변신 로봇',
         price: 5000,
@@ -84,15 +75,7 @@ describe('인증이 필요한 상품 API 통합 테스트', () => {
     });
 
     test('신규 상품 등록 필수값 누락 : name', async () => {
-      const agent = request.agent(app);
-
-      const user = await agent
-        .post('/auth/login')
-        .send({ email: 'test@test.com', password: 'qwer1234' });
-
-      expect(user.status).toBe(200);
-
-      const productResponse = await agent.post('/products').send({
+      const productResponse = await agent1.post('/products').send({
         name: '',
         description: '어린이용 변신 로봇',
         price: 5000,
@@ -104,15 +87,7 @@ describe('인증이 필요한 상품 API 통합 테스트', () => {
     });
 
     test('신규 상품 등록 필수값 누락 : description', async () => {
-      const agent = request.agent(app);
-
-      const user = await agent
-        .post('/auth/login')
-        .send({ email: 'test@test.com', password: 'qwer1234' });
-
-      expect(user.status).toBe(200);
-
-      const productResponse = await agent.post('/products').send({
+      const productResponse = await agent1.post('/products').send({
         name: '로봇',
         description: '',
         price: 5000,
@@ -124,15 +99,7 @@ describe('인증이 필요한 상품 API 통합 테스트', () => {
     });
 
     test('신규 상품 등록 필수값 누락 : price', async () => {
-      const agent = request.agent(app);
-
-      const user = await agent
-        .post('/auth/login')
-        .send({ email: 'test@test.com', password: 'qwer1234' });
-
-      expect(user.status).toBe(200);
-
-      const productResponse = await agent.post('/products').send({
+      const productResponse = await agent1.post('/products').send({
         name: '로봇',
         description: '어린이용 변신 로봇',
         price: '',
@@ -146,15 +113,7 @@ describe('인증이 필요한 상품 API 통합 테스트', () => {
 
   describe('인증을 거친 후 상품 목록 조회 GET /products', () => {
     test('상품이 없는 경우, 빈 배열 반환', async () => {
-      const agent = request.agent(app);
-
-      const user = await agent
-        .post('/auth/login')
-        .send({ email: 'test@test.com', password: 'qwer1234' });
-
-      expect(user.status).toBe(200);
-
-      const getResponse = await agent.get('/products');
+      const getResponse = await agent1.get('/products');
 
       expect(getResponse.status).toBe(200);
       expect(getResponse.body.list).toEqual([]);
@@ -164,15 +123,7 @@ describe('인증이 필요한 상품 API 통합 테스트', () => {
     test('상품이 있는 경우, 전체 목록 조회', async () => {
       await createProductsWithUsers(userSample, productSample);
 
-      const agent = request.agent(app);
-
-      const user = await agent
-        .post('/auth/login')
-        .send({ email: 'test@test.com', password: 'qwer1234' });
-
-      expect(user.status).toBe(200);
-
-      const getResponse = await agent.get('/products');
+      const getResponse = await agent1.get('/products');
 
       expect(getResponse.status).toBe(200);
       expect(getResponse.body.list.length).toBe(10);
@@ -182,10 +133,7 @@ describe('인증이 필요한 상품 API 통합 테스트', () => {
 
   describe('인증을 거친 후 상품 상세 조회 GET /products/:id', () => {
     test('생성자와 관계 없이 상세 조회 가능', async () => {
-      const agent = request.agent(app);
-      await agent.post('/auth/login').send({ email: 'test@test.com', password: 'qwer1234' });
-
-      const product = await agent.post('/products').send({
+      const product = await agent1.post('/products').send({
         name: '로봇',
         description: '어린이용 변신 로봇',
         price: 5000,
@@ -193,20 +141,14 @@ describe('인증이 필요한 상품 API 통합 테스트', () => {
         images: ['img1.jpg', 'img1.png'],
       });
 
-      await agent.post('/auth/logout');
-      await agent.post('/auth/login').send({ email: 'test2@test.com', password: 'qwer1234' });
-
-      const getResponse = await agent.get(`/products/${product.body.id}`);
+      const getResponse = await agent2.get(`/products/${product.body.id}`);
 
       expect(getResponse.status).toBe(200);
       expect(getResponse.body.name).toBe('로봇');
     });
 
     test('존재하지 않는 상품 상세 조회', async () => {
-      const agent = request.agent(app);
-      await agent.post('/auth/login').send({ email: 'test@test.com', password: 'qwer1234' });
-
-      const product = await agent.post('/products').send({
+      const product = await agent1.post('/products').send({
         name: '로봇',
         description: '어린이용 변신 로봇',
         price: 5000,
@@ -214,7 +156,7 @@ describe('인증이 필요한 상품 API 통합 테스트', () => {
         images: ['img1.jpg', 'img1.png'],
       });
 
-      const getResponse = await agent.get(`/products/${product.body.id + 1}`);
+      const getResponse = await agent1.get(`/products/${product.body.id + 1}`);
 
       expect(getResponse.status).toBe(404);
       expect(getResponse.body.message).toBe(`product with id ${product.body.id + 1} not found`);
@@ -223,47 +165,33 @@ describe('인증이 필요한 상품 API 통합 테스트', () => {
 
   describe('인증을 거친 후 상품 수정 PATCH /products/:id', () => {
     test('상품 정보 수정 : name', async () => {
-      const agent = request.agent(app);
-      await agent.post('/auth/login').send({ email: 'test@test.com', password: 'qwer1234' });
-
-      const user = await prismaClient.user.findUnique({ where: { email: 'test@test.com' } });
-
-      const product = await prismaClient.product.create({
-        data: {
-          name: '로봇',
-          description: '어린이용 변신 로봇',
-          price: 5000,
-          tags: ['어린이', '장난감'],
-          images: ['img1.jpg', 'img1.png'],
-          userId: user!.id,
-        },
+      const product = await agent1.post('/products').send({
+        name: '로봇',
+        description: '어린이용 변신 로봇',
+        price: 5000,
+        tags: ['어린이', '장난감'],
+        images: ['img1.jpg', 'img1.png'],
       });
 
-      const updateResponse = await agent.patch(`/products/${product.id}`).send({ name: '인형' });
+      const updateResponse = await agent1
+        .patch(`/products/${product.body.id}`)
+        .send({ name: '인형' });
 
       expect(updateResponse.status).toBe(201);
       expect(updateResponse.body.name).toBe('인형');
     });
 
     test('상품 정보 수정 : description', async () => {
-      const agent = request.agent(app);
-      await agent.post('/auth/login').send({ email: 'test@test.com', password: 'qwer1234' });
-
-      const user = await prismaClient.user.findUnique({ where: { email: 'test@test.com' } });
-
-      const product = await prismaClient.product.create({
-        data: {
-          name: '로봇',
-          description: '어린이용 변신 로봇',
-          price: 5000,
-          tags: ['어린이', '장난감'],
-          images: ['img1.jpg', 'img1.png'],
-          userId: user!.id,
-        },
+      const product = await agent1.post('/products').send({
+        name: '로봇',
+        description: '어린이용 변신 로봇',
+        price: 5000,
+        tags: ['어린이', '장난감'],
+        images: ['img1.jpg', 'img1.png'],
       });
 
-      const updateResponse = await agent
-        .patch(`/products/${product.id}`)
+      const updateResponse = await agent1
+        .patch(`/products/${product.body.id}`)
         .send({ description: '어른용 조립 로봇' });
 
       expect(updateResponse.status).toBe(201);
@@ -271,24 +199,16 @@ describe('인증이 필요한 상품 API 통합 테스트', () => {
     });
 
     test('상품 정보 수정 : price', async () => {
-      const agent = request.agent(app);
-      await agent.post('/auth/login').send({ email: 'test@test.com', password: 'qwer1234' });
-
-      const user = await prismaClient.user.findUnique({ where: { email: 'test@test.com' } });
-
-      const product = await prismaClient.product.create({
-        data: {
-          name: '로봇',
-          description: '어린이용 변신 로봇',
-          price: 5000,
-          tags: ['어린이', '장난감'],
-          images: ['img1.jpg', 'img1.png'],
-          userId: user!.id,
-        },
+      const product = await agent1.post('/products').send({
+        name: '로봇',
+        description: '어린이용 변신 로봇',
+        price: 5000,
+        tags: ['어린이', '장난감'],
+        images: ['img1.jpg', 'img1.png'],
       });
 
-      const updateResponse = await agent
-        .patch(`/products/${product.id}`)
+      const updateResponse = await agent1
+        .patch(`/products/${product.body.id}`)
         .send({ price: 100000000 });
 
       expect(updateResponse.status).toBe(201);
@@ -296,24 +216,16 @@ describe('인증이 필요한 상품 API 통합 테스트', () => {
     });
 
     test('상품 정보 수정 : tags', async () => {
-      const agent = request.agent(app);
-      await agent.post('/auth/login').send({ email: 'test@test.com', password: 'qwer1234' });
-
-      const user = await prismaClient.user.findUnique({ where: { email: 'test@test.com' } });
-
-      const product = await prismaClient.product.create({
-        data: {
-          name: '로봇',
-          description: '어린이용 변신 로봇',
-          price: 5000,
-          tags: ['어린이', '장난감'],
-          images: ['img1.jpg', 'img1.png'],
-          userId: user!.id,
-        },
+      const product = await agent1.post('/products').send({
+        name: '로봇',
+        description: '어린이용 변신 로봇',
+        price: 5000,
+        tags: ['어린이', '장난감'],
+        images: ['img1.jpg', 'img1.png'],
       });
 
-      const updateResponse = await agent
-        .patch(`/products/${product.id}`)
+      const updateResponse = await agent1
+        .patch(`/products/${product.body.id}`)
         .send({ tags: ['조립용', '손조심'] });
 
       expect(updateResponse.status).toBe(201);
@@ -321,24 +233,16 @@ describe('인증이 필요한 상품 API 통합 테스트', () => {
     });
 
     test('상품 정보 수정 : images', async () => {
-      const agent = request.agent(app);
-      await agent.post('/auth/login').send({ email: 'test@test.com', password: 'qwer1234' });
-
-      const user = await prismaClient.user.findUnique({ where: { email: 'test@test.com' } });
-
-      const product = await prismaClient.product.create({
-        data: {
-          name: '로봇',
-          description: '어린이용 변신 로봇',
-          price: 5000,
-          tags: ['어린이', '장난감'],
-          images: ['img1.jpg', 'img1.png'],
-          userId: user!.id,
-        },
+      const product = await agent1.post('/products').send({
+        name: '로봇',
+        description: '어린이용 변신 로봇',
+        price: 5000,
+        tags: ['어린이', '장난감'],
+        images: ['img1.jpg', 'img1.png'],
       });
 
-      const updateResponse = await agent
-        .patch(`/products/${product.id}`)
+      const updateResponse = await agent1
+        .patch(`/products/${product.body.id}`)
         .send({ images: ['img3.jpg'] });
 
       expect(updateResponse.status).toBe(201);
@@ -346,54 +250,33 @@ describe('인증이 필요한 상품 API 통합 테스트', () => {
     });
 
     test('상품 정보 수정 불가 : 잘못된 상품 id', async () => {
-      const agent = request.agent(app);
-      await agent.post('/auth/login').send({ email: 'test@test.com', password: 'qwer1234' });
-
-      const user = await prismaClient.user.findUnique({ where: { email: 'test@test.com' } });
-
-      const product = await prismaClient.product.create({
-        data: {
-          name: '로봇',
-          description: '어린이용 변신 로봇',
-          price: 5000,
-          tags: ['어린이', '장난감'],
-          images: ['img1.jpg', 'img1.png'],
-          userId: user!.id,
-        },
+      const product = await agent1.post('/products').send({
+        name: '로봇',
+        description: '어린이용 변신 로봇',
+        price: 5000,
+        tags: ['어린이', '장난감'],
+        images: ['img1.jpg', 'img1.png'],
       });
 
-      const updateResponse = await agent
-        .patch(`/products/${product.id + 1}`)
+      const updateResponse = await agent1
+        .patch(`/products/${product.body.id + 1}`)
         .send({ name: '수정불가' });
 
       expect(updateResponse.status).toBe(404);
-      expect(updateResponse.body.message).toBe(`product with id ${product.id + 1} not found`);
+      expect(updateResponse.body.message).toBe(`product with id ${product.body.id + 1} not found`);
     });
 
     test('상품 정보 수정 불가 : 현재 상품을 등록하지 않은 다른 사용자', async () => {
-      const agent = request.agent(app);
-      await agent.post('/auth/login').send({ email: 'test@test.com', password: 'qwer1234' });
-
-      const user = await prismaClient.user.findUnique({ where: { email: 'test@test.com' } });
-
-      const product = await prismaClient.product.create({
-        data: {
-          name: '로봇',
-          description: '어린이용 변신 로봇',
-          price: 5000,
-          tags: ['어린이', '장난감'],
-          images: ['img1.jpg', 'img1.png'],
-          userId: user!.id,
-        },
+      const product = await agent1.post('/products').send({
+        name: '로봇',
+        description: '어린이용 변신 로봇',
+        price: 5000,
+        tags: ['어린이', '장난감'],
+        images: ['img1.jpg', 'img1.png'],
       });
 
-      await agent.post('/auth/logout');
-
-      // 다른 사용자로 로그인
-      await agent.post('/auth/login').send({ email: 'test2@test.com', password: 'qwer1234' });
-
-      const updateResponse = await agent
-        .patch(`/products/${product.id}`)
+      const updateResponse = await agent2
+        .patch(`/products/${product.body.id}`)
         .send({ name: '다른 사용자' });
 
       expect(updateResponse.status).toBe(403);
@@ -403,72 +286,44 @@ describe('인증이 필요한 상품 API 통합 테스트', () => {
 
   describe('인증을 거친 후 상품 삭제 DELETE /products/:id', () => {
     test('상품 삭제', async () => {
-      const agent = request.agent(app);
-      await agent.post('/auth/login').send({ email: 'test@test.com', password: 'qwer1234' });
-
-      const user = await prismaClient.user.findUnique({ where: { email: 'test@test.com' } });
-
-      const product = await prismaClient.product.create({
-        data: {
-          name: '로봇',
-          description: '어린이용 변신 로봇',
-          price: 5000,
-          tags: ['어린이', '장난감'],
-          images: ['img1.jpg', 'img1.png'],
-          userId: user!.id,
-        },
+      const product = await agent1.post('/products').send({
+        name: '로봇',
+        description: '어린이용 변신 로봇',
+        price: 5000,
+        tags: ['어린이', '장난감'],
+        images: ['img1.jpg', 'img1.png'],
       });
 
-      const deleteResponse = await agent.delete(`/products/${product.id}`);
+      const deleteResponse = await agent1.delete(`/products/${product.body.id}`);
 
       expect(deleteResponse.status).toBe(204);
     });
 
     test('상품 삭제 불가 : 잘못된 상품 id', async () => {
-      const agent = request.agent(app);
-      await agent.post('/auth/login').send({ email: 'test@test.com', password: 'qwer1234' });
-
-      const user = await prismaClient.user.findUnique({ where: { email: 'test@test.com' } });
-
-      const product = await prismaClient.product.create({
-        data: {
-          name: '로봇',
-          description: '어린이용 변신 로봇',
-          price: 5000,
-          tags: ['어린이', '장난감'],
-          images: ['img1.jpg', 'img1.png'],
-          userId: user!.id,
-        },
+      const product = await agent1.post('/products').send({
+        name: '로봇',
+        description: '어린이용 변신 로봇',
+        price: 5000,
+        tags: ['어린이', '장난감'],
+        images: ['img1.jpg', 'img1.png'],
       });
 
-      const deleteResponse = await agent.delete(`/products/${product.id + 1}`);
+      const deleteResponse = await agent1.delete(`/products/${product.body.id + 1}`);
 
       expect(deleteResponse.status).toBe(404);
-      expect(deleteResponse.body.message).toBe(`product with id ${product.id + 1} not found`);
+      expect(deleteResponse.body.message).toBe(`product with id ${product.body.id + 1} not found`);
     });
 
     test('상품 삭제 불가 : 현재 상품을 등록하지 않은 다른 사용자 ', async () => {
-      const agent = request.agent(app);
-      await agent.post('/auth/login').send({ email: 'test@test.com', password: 'qwer1234' });
-
-      const user = await prismaClient.user.findUnique({ where: { email: 'test@test.com' } });
-
-      const product = await prismaClient.product.create({
-        data: {
-          name: '로봇',
-          description: '어린이용 변신 로봇',
-          price: 5000,
-          tags: ['어린이', '장난감'],
-          images: ['img1.jpg', 'img1.png'],
-          userId: user!.id,
-        },
+      const product = await agent1.post('/products').send({
+        name: '로봇',
+        description: '어린이용 변신 로봇',
+        price: 5000,
+        tags: ['어린이', '장난감'],
+        images: ['img1.jpg', 'img1.png'],
       });
 
-      await agent.post('/auth/logout');
-
-      await agent.post('/auth/login').send({ email: 'test2@test.com', password: 'qwer1234' });
-
-      const deleteResponse = await agent.delete(`/products/${product.id}`);
+      const deleteResponse = await agent2.delete(`/products/${product.body.id}`);
 
       expect(deleteResponse.status).toBe(403);
       expect(deleteResponse.body.message).toBe('Should be the owner of the product');

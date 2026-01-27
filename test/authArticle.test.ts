fb3b9b0b2fd275180d9417a08dc375b70cbb42e0
kimjs <kimjs910522@gmail.com>
2026-01-27 11:22:39 +0900

    refactor : authArticle 리팩토링

--- test/authArticle.test.ts
+++ test/authArticle.test.ts
@@ -2,6 +2,9 @@ import request from 'supertest';
 import { prismaClient } from '@lib/prismaClient';
 import app from '@/app';
 
+const agent1 = request.agent(app);
+const agent2 = request.agent(app);
+
 describe('인증이 필요한 게시글 API 통합 테스트', () => {
   console.log('auth article run!!');
 
@@ -18,19 +21,23 @@ describe('인증이 필요한 게시글 API 통합 테스트', () => {
     await prismaClient.user.deleteMany();
 
     // 사용자 식별 테스트를 위하여 임의로 user data 2개 입력
-    await request(app).post('/auth/register').send({
+    await agent1.post('/auth/register').send({
       email: 'test@test.com',
       nickname: 'tester',
       password: 'qwer1234',
       image: 'test.jpg',
     });
 
-    await request(app).post('/auth/register').send({
+    await agent1.post('/auth/login').send({ email: 'test@test.com', password: 'qwer1234' });
+
+    await agent2.post('/auth/register').send({
       email: 'test2@test.com',
       nickname: 'tester2',
       password: 'qwer1234',
       image: 'test.jpg',
     });
+
+    await agent2.post('/auth/login').send({ email: 'test2@test.com', password: 'qwer1234' });
   });
 
   afterAll(async () => {
@@ -39,15 +46,7 @@ describe('인증이 필요한 게시글 API 통합 테스트', () => {
 
   describe('인증을 거친 후 게시글 생성 POST /articles', () => {
     test('신규 게시글 작성 : title, content, image', async () => {
-      const agent = request.agent(app);
-
-      const user = await agent
-        .post('/auth/login')
-        .send({ email: 'test@test.com', password: 'qwer1234' });
-
-      expect(user.status).toBe(200);
-
-      const articleResponse = await agent.post('/articles').send({
+      const articleResponse = await agent1.post('/articles').send({
         title: '신규 게시글',
         content: '첫번째 게시글 입니다',
         image: 'img.jpg',
@@ -55,21 +54,38 @@ describe('인증이 필요한 게시글 API 통합 테스트', () => {
       });
 
       expect(articleResponse.status).toBe(201);
+      expect(articleResponse.body.title).toBe('신규 게시글');
     });
 
     test('신규 게시글 작성 : image null', async () => {
-      const agent = request.agent(app);
-
-      const user = await agent
-        .post('/auth/login')
-        .send({ email: 'test@test.com', password: 'qwer1234' });
-
-      expect(user.status).toBe(200);
-
-      const articleResponse = await agent.post('/articles').send({
+      const articleResponse = await agent1.post('/articles').send({
         title: '신규 게시글 2',
         content: '이미지가 없는 게시글 입니다',
+        image: '',
       });
+
+      expect(articleResponse.status).toBe(201);
+      expect(articleResponse.body.title).toBe('신규 게시글 2');
+    });
+
+    test('신규 게시글 필수값 누락 : title', async () => {
+      const articleResponse = await agent1.post('/articles').send({
+        title: '',
+        content: '첫번째 게시글 입니다',
+        image: 'img.jpg',
+      });
+
+      expect(articleResponse.status).toBe(400);
+    });
+
+    test('신규 게시글 필수값 누락 : title', async () => {
+      const articleResponse = await agent1.post('/articles').send({
+        title: '신규 게시글',
+        content: '',
+        image: 'img.jpg',
+      });
+
+      expect(articleResponse.status).toBe(400);
     });
   });
 
@@ -78,16 +94,12 @@ describe('인증이 필요한 게시글 API 통합 테스트', () => {
 
   describe('인증을 거친 후 게시글 수정 PATCH /articles/:id', () => {
     test('게시글 수정 : title', async () => {
-      const agent = request.agent(app);
-      await agent.post('/auth/login').send({ email: 'test@test.com', password: 'qwer1234' });
+      const article = await agent1
+        .post('/articles')
+        .send({ title: '게시글', content: '내용', image: 'img.jpg' });
 
-      const user = await prismaClient.user.findUnique({ where: { email: 'test@test.com' } });
-      const article = await prismaClient.article.create({
-        data: { title: '게시글', content: '내용', userId: user!.id },
-      });
-
-      const updateResponse = await agent
-        .patch(`/articles/${article.id}`)
+      const updateResponse = await agent1
+        .patch(`/articles/${article.body.id}`)
         .send({ title: '게시글 수정' });
 
       expect(updateResponse.status).toBe(200);
@@ -95,16 +107,12 @@ describe('인증이 필요한 게시글 API 통합 테스트', () => {
     });
 
     test('게시글 수정 : content', async () => {
-      const agent = request.agent(app);
-      await agent.post('/auth/login').send({ email: 'test@test.com', password: 'qwer1234' });
+      const article = await agent1
+        .post('/articles')
+        .send({ title: '게시글', content: '내용', image: 'img.jpg' });
 
-      const user = await prismaClient.user.findUnique({ where: { email: 'test@test.com' } });
-      const article = await prismaClient.article.create({
-        data: { title: '게시글', content: '내용', userId: user!.id },
-      });
-
-      const updateResponse = await agent
-        .patch(`/articles/${article.id}`)
+      const updateResponse = await agent1
+        .patch(`/articles/${article.body.id}`)
         .send({ content: '내용 수정' });
 
       expect(updateResponse.status).toBe(200);
@@ -112,16 +120,12 @@ describe('인증이 필요한 게시글 API 통합 테스트', () => {
     });
 
     test('게시글 수정 : image', async () => {
-      const agent = request.agent(app);
-      await agent.post('/auth/login').send({ email: 'test@test.com', password: 'qwer1234' });
+      const article = await agent1
+        .post('/articles')
+        .send({ title: '게시글', content: '내용', image: 'img.jpg' });
 
-      const user = await prismaClient.user.findUnique({ where: { email: 'test@test.com' } });
-      const article = await prismaClient.article.create({
-        data: { title: '게시글', content: '내용', image: 'img.jpg', userId: user!.id },
-      });
-
-      const updateResponse = await agent
-        .patch(`/articles/${article.id}`)
+      const updateResponse = await agent1
+        .patch(`/articles/${article.body.id}`)
         .send({ image: 'img123.jpg' });
 
       expect(updateResponse.status).toBe(200);
@@ -129,39 +133,26 @@ describe('인증이 필요한 게시글 API 통합 테스트', () => {
     });
 
     test('게시글 수정 불가 : 잘못된 게시글 id', async () => {
-      const agent = request.agent(app);
-      await agent.post('/auth/login').send({ email: 'test@test.com', password: 'qwer1234' });
+      const article = await agent1
+        .post('/articles')
+        .send({ title: '게시글', content: '내용', image: 'img.jpg' });
 
-      const user = await prismaClient.user.findUnique({ where: { email: 'test@test.com' } });
-
-      const article = await prismaClient.article.create({
-        data: { title: '게시글', content: '내용', userId: user!.id },
-      });
-
-      const updateResponse = await agent
-        .patch(`/articles/${article.id + 1}`)
+      const updateResponse = await agent1
+        .patch(`/articles/${article.body.id + 1}`)
         .send({ title: '게시글 id 오류' });
 
       expect(updateResponse.status).toBe(404);
-      expect(updateResponse.body.message).toBe(`article with id ${article.id + 1} not found`);
+      expect(updateResponse.body.message).toBe(`article with id ${article.body.id + 1} not found`);
     });
 
-    test('게시글 수정 불가 : 다른 로그인 사용자', async () => {
-      const agent = request.agent(app);
-      await agent.post('/auth/login').send({ email: 'test@test.com', password: 'qwer1234' });
+    test('게시글 수정 불가 : 현재 글을 작성하지 않은 다른 사용자', async () => {
+      const article = await agent1
+        .post('/articles')
+        .send({ title: '게시글', content: '내용', image: 'img.jpg' });
 
-      const user = await prismaClient.user.findUnique({ where: { email: 'test@test.com' } });
-      const article = await prismaClient.article.create({
-        data: { title: '게시글', content: '내용', userId: user!.id },
-      });
-
-      await agent.post('/auth/logout');
-
-      // 다른 사용자로 로그인
-      await agent.post('/auth/login').send({ email: 'test2@test.com', password: 'qwer1234' });
-
-      const updateResponse = await agent
-        .patch(`/articles/${article.id}`)
+      // 다른 사용자(agent2)로 게시글 수정
+      const updateResponse = await agent2
+        .patch(`/articles/${article.body.id}`)
         .send({ title: '다른사람' });
 
       expect(updateResponse.status).toBe(403);
@@ -171,49 +162,33 @@ describe('인증이 필요한 게시글 API 통합 테스트', () => {
 
   describe('인증을 거친 후 게시글 삭제 DELETE /articles/:id', () => {
     test('게시글 삭제', async () => {
-      const agent = request.agent(app);
-      await agent.post('/auth/login').send({ email: 'test@test.com', password: 'qwer1234' });
+      const article = await agent1
+        .post('/articles')
+        .send({ title: '게시글', content: '내용', image: 'img.jpg' });
 
-      const user = await prismaClient.user.findUnique({ where: { email: 'test@test.com' } });
-      const article = await prismaClient.article.create({
-        data: { title: '게시글', content: '내용', image: 'img.jpg', userId: user!.id },
-      });
-
-      const deleteResponse = await agent.delete(`/articles/${article.id}`);
+      const deleteResponse = await agent1.delete(`/articles/${article.body.id}`);
 
       expect(deleteResponse.status).toBe(204);
     });
 
     test('게시글 삭제 불가 : 잘못된 게시글 id', async () => {
-      const agent = request.agent(app);
-      await agent.post('/auth/login').send({ email: 'test@test.com', password: 'qwer1234' });
+      const article = await agent1
+        .post('/articles')
+        .send({ title: '게시글', content: '내용', image: 'img.jpg' });
 
-      const user = await prismaClient.user.findUnique({ where: { email: 'test@test.com' } });
-      const article = await prismaClient.article.create({
-        data: { title: '게시글', content: '내용', image: 'img.jpg', userId: user!.id },
-      });
-
-      const deleteResponse = await agent.delete(`/articles/${article.id + 1}`);
+      const deleteResponse = await agent1.delete(`/articles/${article.body.id + 1}`);
 
       expect(deleteResponse.status).toBe(404);
-      expect(deleteResponse.body.message).toBe(`article with id ${article.id + 1} not found`);
+      expect(deleteResponse.body.message).toBe(`article with id ${article.body.id + 1} not found`);
     });
 
-    test('게시글 삭제 불가 : 다른 로그인 사용자', async () => {
-      const agent = request.agent(app);
-      await agent.post('/auth/login').send({ email: 'test@test.com', password: 'qwer1234' });
+    test('게시글 삭제 불가 : 현재 글을 작성하지 않은 다른 사용자', async () => {
+      const article = await agent1
+        .post('/articles')
+        .send({ title: '게시글', content: '내용', image: 'img.jpg' });
 
-      const user = await prismaClient.user.findUnique({ where: { email: 'test@test.com' } });
-      const article = await prismaClient.article.create({
-        data: { title: '게시글', content: '내용', image: 'img.jpg', userId: user!.id },
-      });
-
-      await agent.post('/auth/logout');
-
-      // 다른 사용자로 로그인
-      await agent.post('/auth/login').send({ email: 'test2@test.com', password: 'qwer1234' });
-
-      const deleteResponse = await agent.delete(`/articles/${article.id}`);
+      // 다른 사용자(agent2)로 게시글 삭제
+      const deleteResponse = await agent2.delete(`/articles/${article.body.id}`);
 
       expect(deleteResponse.status).toBe(403);
       expect(deleteResponse.body.message).toBe('Should be the owner of the article');

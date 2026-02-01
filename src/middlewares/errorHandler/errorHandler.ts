ff9471b04bc54aac8e69430c5267d56c160c5320
오윤 <passfile2@naver.com>
2026-02-01 23:59:41 +0900

    refactor: 테스트시 오류 및 필요없는 출력 제거

--- src/middlewares/errorHandler/errorHandler.ts
+++ src/middlewares/errorHandler/errorHandler.ts
@@ -28,7 +28,9 @@ function globalErrorHandler(
   res: Response,
   next: NextFunction
 ) {
-  console.log(err);
+  if (process.env.NODE_ENV !== 'test') {
+    console.log(err);
+  }
   if (err instanceof Prisma.PrismaClientKnownRequestError) {
     if (err.code === 'P2002') {
       return res.status(400).json({ message: '이미 존재하는 데이터입니다.' });

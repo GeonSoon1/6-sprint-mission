532731dd5f16021dcc313ad5d25ff64e3ef434e4
Yeongjun <ing08114@gmail.com>
2026-02-02 17:34:34 +0900

    s3 추가에 따른 업로드 파일 수정

--- src/router/uploadRouter.ts
+++ src/router/uploadRouter.ts
@@ -1,18 +1,23 @@
 import express from 'express';
-import multer from 'multer';
+import { upload } from '../middleware/upload';
 
 const uploadRouter = express.Router();
-
-const upload = multer({ dest: 'uploads/' });
 
 uploadRouter.post('/', upload.single('attachment'), (req, res) => {
   console.log(req.file);
   if (!req.file) {
     return res.status(400).json({ error: 'No file uploaded' });
   }
-  const filename = req.file.filename;
-  const path = `/files/${filename}`;
-  res.json({ path });
+  /**
+   * S3일 때: req.file.location
+   * 로컬일 때: req.file.filename
+   */
+  const filePath =
+    process.env.NODE_ENV === 'production'
+      ? (req.file as any).location
+      : `/files/${req.file.filename}`;
+
+  res.json({ path: filePath });
 });
 
 export default uploadRouter;
